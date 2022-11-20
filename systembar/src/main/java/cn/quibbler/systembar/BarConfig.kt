df624da576f8e7d2153c76ae104176b957abf27f
quibbler01 <fluent0418@gmail.com>
2022-11-21 00:14:56 +0800

    add BarConfig.kt

---
+++ systembar/src/main/java/cn/quibbler/systembar/BarConfig.kt
@@ -0,0 +1,149 @@
+package cn.quibbler.systembar
+
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.os.Build
+import android.util.DisplayMetrics
+import android.util.TypedValue
+import android.view.View
+import kotlin.math.min
+
+class BarConfig(activity: Activity) {
+
+    companion object {
+
+        fun getInternalDimensionSize(context: Context, key: String): Int {
+            val result = 0
+            try {
+                val resourceId = Resources.getSystem().getIdentifier(key, "dimen", "android")
+                if (resourceId > 0) {
+                    val sizeOne = context.resources.getDimensionPixelSize(resourceId)
+                    val sizeTwo = Resources.getSystem().getDimensionPixelSize(resourceId)
+
+                    if (sizeTwo >= sizeOne && !(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
+                                key != IMMERSION_STATUS_BAR_HEIGHT)
+                    ) {
+                        return sizeTwo
+                    } else {
+                        val densityOne = context.resources.displayMetrics.density
+                        val densityTwo = Resources.getSystem().displayMetrics.density
+                        val f = sizeOne * densityTwo / densityOne
+                        return (if (f >= 0) (f + 0.5f) else (f - 0.5f)).toInt()
+                    }
+                }
+            } catch (ignored: Resources.NotFoundException) {
+                return 0
+            }
+            return result
+        }
+
+        fun getNavigationBarHeightInternal(context: Context): Int {
+            var key: String = if (context.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                IMMERSION_NAVIGATION_BAR_HEIGHT
+            } else {
+                IMMERSION_NAVIGATION_BAR_HEIGHT_LANDSCAPE
+            }
+            return getInternalDimensionSize(context, key)
+        }
+
+        fun getNavigationBarWidthInternal(context: Context): Int = getInternalDimensionSize(context, IMMERSION_NAVIGATION_BAR_WIDTH)
+
+    }
+
+    private val mStatusBarHeight: Int
+    private val mActionBarHeight: Int
+    private val mHasNavigationBar: Boolean
+    private val mNavigationBarHeight: Int
+    private val mNavigationBarWidth: Int
+    private val mInPortrait: Boolean
+    private val mSmallestWidthDp: Float
+
+    init {
+        val res = activity.resources
+        mInPortrait = res.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+        mSmallestWidthDp = getSmallestWidthDp(activity)
+        mStatusBarHeight = getInternalDimensionSize(activity, IMMERSION_STATUS_BAR_HEIGHT)
+        mActionBarHeight = getActionBarHeight(activity)
+        mNavigationBarHeight = getNavigationBarHeight(activity)
+        mNavigationBarWidth = getNavigationBarWidth(activity)
+        mHasNavigationBar = (mNavigationBarHeight > 0)
+    }
+
+    private fun getActionBarHeight(activity: Activity): Int {
+        var result = 0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            val actionBar: View? = activity.window.findViewById(R.id.action_bar_container)
+            actionBar?.let {
+                result = it.measuredHeight
+            }
+            if (result == 0) {
+                val tv = TypedValue()
+                activity.theme.resolveAttribute(android.R.attr.actionBarSize, tv, true)
+                result = TypedValue.complexToDimensionPixelSize(tv.data, activity.resources.displayMetrics)
+            }
+        }
+        return result
+    }
+
+    private fun getNavigationBarHeight(context: Context): Int {
+        var result = 0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            if (hasNavBar(context as Activity)) {
+                return getNavigationBarHeightInternal(context)
+            }
+        }
+        return result
+    }
+
+    private fun getNavigationBarWidth(context: Context): Int {
+        var result = 0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            if (hasNavBar(context as Activity)) {
+                return getInternalDimensionSize(context, IMMERSION_NAVIGATION_BAR_WIDTH)
+            }
+        }
+        return result
+    }
+
+    private fun hasNavBar(activity: Activity): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            val gestureBean = getGestureBean(activity)
+            if (!gestureBean.checkNavigation && gestureBean.isGesture) {
+                return false
+            }
+        }
+
+        //Other mobile phones judge whether the real height of the screen is the same as the display height
+        val windowManager = activity.windowManager
+        val d = windowManager.defaultDisplay
+
+        val realDisplayMetrics = DisplayMetrics()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            d.getRealMetrics(realDisplayMetrics)
+        }
+
+        val realHeight = realDisplayMetrics.heightPixels
+        val realWidth = realDisplayMetrics.widthPixels
+        val displayMetrics = DisplayMetrics()
+        d.getRealMetrics(displayMetrics)
+
+        val displayHeight = displayMetrics.heightPixels
+        val displayWidth = displayMetrics.widthPixels
+        return (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0
+    }
+
+    private fun getSmallestWidthDp(activity: Activity): Float {
+        val metric = DisplayMetrics()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            activity.windowManager.defaultDisplay.getRealMetrics(metric)
+        } else {
+            activity.windowManager.defaultDisplay.getMetrics(metric)
+        }
+        val widthDp = metric.widthPixels / metric.density
+        val heightDp = metric.heightPixels / metric.density
+        return min(widthDp, heightDp)
+    }
+
+}
