00fe9ae98b50a7a500d272a8c14930bd50f4eef8
quibbler01 <fluent0418@gmail.com>
2022-11-26 00:03:32 +0800

    add BarProperties.kt

---
+++ systembar/src/main/java/cn/quibbler/systembar/BarProperties.kt
@@ -0,0 +1,58 @@
+package cn.quibbler.systembar
+
+/**
+ * Bar related information
+ */
+class BarProperties {
+
+    /**
+     * Vertical screen or not
+     */
+    var portrait = false
+
+    /**
+     * Whether it is left horizontal screen
+     */
+    var landscapeLeft = false
+
+    /**
+     * Is it the right horizontal screen
+     */
+    var landscapeRight = false
+
+    /**
+     * Is it Liu Haiping
+     */
+    var notchScreen = false
+
+    /**
+     * Is there a navigation bar
+     */
+    var hasNavigationBar = false
+
+    /**
+     * The height of the status bar may be different for the horizontal and vertical screens
+     */
+    var statusBarHeight = 0
+
+    /**
+     * Navigation bar height
+     */
+    var navigationBarHeight = 0
+
+    /**
+     * Navigation bar width
+     */
+    var navigationBarWidth = 0
+
+    /**
+     * Liu Haiping Height
+     */
+    var notchHeight = 0
+
+    /**
+     * Action Bar Height
+     */
+    var actionBarHeight = 0
+
+}
