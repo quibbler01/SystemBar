83cf704dcb6393b002f31b23e658ad56c0e5535c
quibbler01 <fluent0418@gmail.com>
2022-11-29 00:08:36 +0800

    add OnBarListener.kt

---
+++ systembar/src/main/java/cn/quibbler/systembar/OnBarListener.kt
@@ -0,0 +1,16 @@
+package cn.quibbler.systembar
+
+/**
+ * The interface On bar listener.
+ *
+ */
+interface OnBarListener {
+
+    /**
+     * On bar info change.
+     *
+     * @param barProperties the bar info
+     */
+    fun onBarChange(barProperties: BarProperties?)
+
+}
