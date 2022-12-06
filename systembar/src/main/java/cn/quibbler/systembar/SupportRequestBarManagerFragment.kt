5b71c43298a684c85e7378a13f8b6c3c0de70a51
quibbler01 <fluent0418@gmail.com>
2022-12-06 22:15:22 +0800

    channge ImmersionBar.kt

--- systembar/src/main/java/cn/quibbler/systembar/SupportRequestBarManagerFragment.kt
+++ systembar/src/main/java/cn/quibbler/systembar/SupportRequestBarManagerFragment.kt
@@ -1,4 +1,50 @@
 package cn.quibbler.systembar
 
-class SupportRequestBarManagerFragment {
+import android.app.Activity
+import android.app.Dialog
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class SupportRequestBarManagerFragment : Fragment() {
+
+    private var mDelegate: ImmersionDelegate? = null
+
+    fun get(a: Any): ImmersionBar? {
+        if (mDelegate == null) {
+            mDelegate = ImmersionDelegate(a)
+        }
+        return mDelegate?.get()
+    }
+
+    fun get(activity: Activity, dialog: Dialog): ImmersionBar? {
+        if (mDelegate == null) {
+            mDelegate = ImmersionDelegate(activity, dialog)
+        }
+        return mDelegate?.get()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mDelegate?.onActivityCreated(resources.configuration)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mDelegate?.onResume()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mDelegate?.let {
+            it.onDestroy()
+            mDelegate = null
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        mDelegate?.onConfigurationChanged(newConfig)
+    }
+
 }
